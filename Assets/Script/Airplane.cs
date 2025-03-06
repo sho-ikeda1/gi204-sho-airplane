3a7b78088ccfd2d136f2fca410c7d79155e98fb2
sho-ikeda1 <sho.iked@bumail.net>
2025-03-06 16:27:22 +0700

    code+button

--- Assets/Script/Airplane.cs
+++ Assets/Script/Airplane.cs
@@ -7,7 +7,10 @@ public class Airplane : MonoBehaviour
     [SerializeField] float liftBooster = 0.5f;
     [SerializeField] float drag = 0.001f;
     [SerializeField] float angularDrag = 0.001f;
-    
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -23,5 +26,15 @@ public class Airplane : MonoBehaviour
         }
         Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
         rb.AddForce(transform.up * lift.magnitude * liftBooster);
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
     }
 }
